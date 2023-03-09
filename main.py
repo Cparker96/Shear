2d96ae3892cc7a0fcf7f8e6b06ab508b583d106c
Cody Parker <codywparker96@gmail.com>
2023-03-08 22:39:40 -0600

    directory structure changes

---
+++ main.py
@@ -0,0 +1,11 @@
+from commands.functions import(
+    auth_header, 
+    token, 
+    url, 
+    bot
+) 
+
+if __name__ == "main":
+    # IMPORT THE FUNCTIONS  
+    # DO THE LOGIC
+    bot.run(token)

9b4ea7268fce792801b45776e509110af247d96d
Cody Parker <codywparker96@gmail.com>
2023-09-07 21:16:52 -0500

    changes idk

--- logs/logger.py
+++ logs/logger.py
@@ -14,26 +14,26 @@ logger = logging.getLogger()
 # CRITIAL ||    50
 
 # creating handlers
-declare_stream_handler = logging.StreamHandler()
+#declare_stream_handler = logging.StreamHandler()
 info_handler = logging.FileHandler('valid_logs.log')
 error_handler_voice = logging.FileHandler('invalid_voice_channel_logs.log')
-declare_stream_handler.setLevel('DEBUG')
+#declare_stream_handler.setLevel('DEBUG')
 info_handler.setLevel('INFO')
 error_handler_voice.setLevel('ERROR')
 
 # creating formatters
-declare_stream_format = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
-info_format = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
-error_format = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
+#declare_stream_format = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
+info_format = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
+error_format = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
 
 # set the formatters
-declare_stream_handler.setFormatter(declare_stream_format)
+#declare_stream_handler.setFormatter(declare_stream_format)
 info_handler.setFormatter(info_format)
 error_handler_voice.setFormatter(error_format)
 
 # add handlers to the logger
-logger.addHandler(declare_stream_handler)
-logger.addHandler(info_format)
+#logger.addHandler(declare_stream_handler)
+logger.addHandler(info_handler)
 logger.addHandler(error_handler_voice)
 
-logger.error('error test')
+

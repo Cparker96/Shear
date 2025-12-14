a2388239af17ea2460386b327431c78754b19234
Cody Parker <codywparker96@gmail.com>
2025-12-14 17:17:13 -0600

    removing registerCommands func

--- main.go
+++ main.go
@@ -68,17 +68,3 @@ func ready(s *discordgo.Session, event *discordgo.Ready) {
 	}
 	fmt.Println()
 }
-
-func registerCommands(s *discordgo.Session, commands []*discordgo.ApplicationCommand) {
-	appID := s.State.User.ID
-	guildID := "1067993028918267985"
-
-	logger.Info("Registering commands...")
-	for _, v := range commands {
-		_, err := s.ApplicationCommandCreate(appID, guildID, v)
-		if err != nil {
-			logger.Error("Cannot create command %s: %v", v.Name, "Error", err)
-		}
-		logger.Info("Successfully registered command: /%s", v.Name)
-	}
-}

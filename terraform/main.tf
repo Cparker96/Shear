5ac940805f2848a87579923b2a7ea04ec8606002
Cody Parker <codywparker96@gmail.com>
2025-12-16 17:35:26 -0600

    parameterizing tf vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,11 +4,11 @@ resource "digitalocean_ssh_key" "ssh_key" {
 }
 
 resource "digitalocean_droplet" "shear-server" {
-  name      = var.server_name
-  image     = var.image
-  region    = var.region
-  size      = var.server_size
-  ssh_keys  = [digitalocean_ssh_key.ssh_key.id]
+  name     = var.server_name
+  image    = var.image
+  region   = var.region
+  size     = var.server_size
+  ssh_keys = [digitalocean_ssh_key.ssh_key.id]
   user_data = templatefile("${path.module}/user_data.tpl", {
     docker_install_script     = file("${path.module}/../scripts/install_docker.sh")
     prometheus_install_script = file("${path.module}/../scripts/install_prometheus.sh")
